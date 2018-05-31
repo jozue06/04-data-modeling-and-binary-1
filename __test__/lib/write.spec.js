c1436a44ba443d20e3c4a680effd323b11772f37
Khoa Huynh <khoawin92@gmail.com>
2018-05-30 21:31:53 -0700

    added portions of cli

--- __test__/lib/write.spec.js
+++ __test__/lib/write.spec.js
@@ -1,12 +1,10 @@
-'use strict';
-
 const reader = require('../../src/lib/reader');
 const CleanBuffer = require('../../src/lib/bitmap');
 const colorFun = require('../../src/lib/colorFun');
 
 const root = __dirname + '/../..';
 
-describe('colorFun module', () => {
+describe('Write module', () => {
 
   // it('should change all the color values to 0, the view will be black', (done) => {
 
@@ -14,6 +12,9 @@ describe('colorFun module', () => {
 
       
   //     let cleaned = new CleanBuffer(data);
+  //     console.log(cleaned.height);
+  //     console.log(cleaned.width);
+  //     console.log(cleaned.numColors);
   //     var black = colorFun(cleaned);
   //     expect(err).toBeNull();
   //     expect(black.colorTable[0]).toBe(0);
--- src/lib/colorFun.js
+++ src/lib/colorFun.js
@@ -1,20 +1,65 @@
 'use strict';
 
-function colorFun(buffer) {
+const changeColors = {};
 
-    let colorTable = buffer.colorTable;
-//   buffer [] = '0x';
-//   buffer[] = '0x';
+changeColors.colorFun = function(buffer) {
 
-for(var i=0;i < colorTable.length;i++){
-    colorTable[i]=0x0;
-    
-}
+  let colorTable = buffer.colorTable;   
 
-return(buffer);
-  
+  for (var i = 0; i < colorTable.length; i++) {
+    // colorTable[i] = 0x0;
+    // colorTable[i] = colorTable[i]*2;
 
-  
-}
+  }
 
-module.exports = colorFun;
+  return (buffer);
+
+
+};
+
+changeColors.invert = function(buffer) {
+
+  let raster = buffer.raster;
+
+  // for (var i = 0; i < raster.length; i+=3) {
+  //   raster[i] = 0xFF - raster[i];
+  //   raster[i+1] = 0xFF - raster[i+1];
+  //   raster[i+2] = 0xFF - raster[i+2];
+  // }
+
+  for (var i = 0; i < raster.length; i+=3) {
+    raster[i] = Math.min(255,raster[i]+50);
+    raster[i+1] = Math.min(255,raster[i+1]+50);
+    raster[i+2] = Math.min(255,raster[i+2]+50);
+  }
+  return(buffer);
+
+};
+
+changeColors.darken = function(buffer) {
+
+  let raster = buffer.raster;
+
+  for (var i = 0; i < raster.length; i+=3) {
+    raster[i] = raster[i]/4;
+    raster[i+1] = raster[i+1]/4;
+    raster[i+2] = raster[i+2]/4;
+  }
+  return(buffer);
+
+};
+
+changeColors.lighten = function(buffer) {
+
+  let raster = buffer.raster;
+
+  for (var i = 0; i < raster.length; i+=3) {
+    raster[i] = Math.min(255,raster[i]+50);
+    raster[i+1] = Math.min(255,raster[i+1]+50);
+    raster[i+2] = Math.min(255,raster[i+2]+50);
+  }
+  return(buffer);
+
+};
+
+module.exports = changeColors;
