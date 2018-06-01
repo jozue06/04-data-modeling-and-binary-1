9e35068cb34a5d60f5d4bbe4168a7983e3ff0afb
Khoa Huynh <khoawin92@gmail.com>
2018-05-31 18:33:09 -0700

    added conditional for color pallete vs non color pallete

--- index.js
+++ index.js
@@ -1,55 +1,14 @@
 'use strict';
 
 
-const reader = require('./src/lib/reader');
-const CleanBuffer = require('./src/lib/bitmap');
-const changeColors = require('./src/lib/colorFun');
-const write = require('./src/lib/write');
-
-const root = __dirname;
+const app = require('./src/app');
 
 let original = process.argv[2];
 let newFile = process.argv[3];
-let transformationLabel = process.argv[4];
-let transformation;
-switch (transformationLabel) {
-case 'invert':
-  transformation = changeColors.invert;
-  break;
+let transformationString = process.argv[4];
 
-case 'darken':
-  transformation = changeColors.darken;
-  break;
+app(original, newFile, transformationString);
 
-case 'lighten':
-  transformation = changeColors.lighten;
-  break;
-
-
-}
-console.log('dir name is dir name', __dirname);
-
-reader(`${root}/assets/${original}`, (err, data) => {
-  if (err) {
-    throw err;
-  } else {
-
-    let cleaned = new CleanBuffer(data);
-
-    var newBuffer = transformation(cleaned);
-    
-    // var newBuffer = transformation(cleaned);
-    write(`${root}/assets/${newFile}`, newBuffer.buffer, (err) => {
-      if (err) throw err;
-      else {
-        console.log('write finished');
-      }
-    });
-
-
-  }
-
-});
 
 
 
